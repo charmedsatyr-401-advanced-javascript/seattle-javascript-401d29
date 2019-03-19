fd54eb740efda00252c099c837230f16be2d6965
John Cokos <highlander.44@gmail.com>
2019-03-18 17:45:41 -0700

    Lab 02 (modified)

--- curriculum/class-02/lab/starter-code/objects/__tests__/vehicle.test.js
+++ curriculum/class-02/lab/starter-code/objects/__tests__/vehicle.test.js
@@ -1,49 +1,84 @@
 'use strict';
 
-const Vehicle = require('../vehicle-constructor.js');
+const VehicleConstructor = require('../vehicle-constructor.js');
+
+let types = ['Constructor'];
 
 describe('Vehicles', () => {
 
   describe('Car', () => {
+    
+    function getCar(type) {
+      switch(type) {
+        case 'Constructor':
+          return new VehicleConstructor.Car('foo');
+        case 'Factory':
+          return new VehicleFactory.Car('foo');
+        case 'Class':
+          return new VehicleClass.Car('foo');
+        default:
+          return {};
+      }
+    }
+    
+    types.forEach( type => {
+      
+      let car = getCar(type);
 
-    let car = new Vehicle.Car('foo');
+      it(`${type} (Car) has 4 wheels`, () => {
+        expect(car.wheels).toEqual(4);
+      });
 
-    it('has 4 wheels', () => {
-      expect(car.wheels).toEqual(4);
-    });
+      it(`${type} (Car) can drive`, () => {
+        expect(car.drive()).toBeTruthy();
+      });
 
-    it('can drive', () => {
-      expect(car.drive()).toBeTruthy();
-    });
+      it(`${type} (Car) can stop`, () => {
+        expect(car.stop()).toBeTruthy();
+      });
 
-    it('can stop', () => {
-      expect(car.stop()).toBeTruthy();
-    });
-
-    it('cannot do a wheelie', () => {
-      expect(car.wheelie).toBeUndefined();
+      it(`${type} (Car) cannot do a wheelie`, () => {
+        expect(car.wheelie).toBeUndefined();
+      }); 
     });
 
   });
 
-  describe('Motorcycle', () => {
+  describe(`Motorcycle`, () => {
 
-    let motorcycle = new Vehicle.Motorcycle('foo');
+    function getMotorcycle(type) {
+      switch(type) {
+        case 'Constructor':
+          return new VehicleConstructor.Motorcycle('foo');
+        case 'Factory':
+          return new VehicleFactory.Motorcycle('foo');
+        case 'Class':
+          return new VehicleClass.Motorcycle('foo');
+        default:
+          return {};
+      }
+    }
 
-    it('has 2 wheels', () => {
-      expect(motorcycle.wheels).toEqual(2);
-    });
+    types.forEach( type => {
 
-    it('can drive', () => {
-      expect(motorcycle.drive()).toBeTruthy();
-    });
+      let motorcycle = getMotorcycle(type);
 
-    it('can stop', () => {
-      expect(motorcycle.stop()).toBeTruthy();
-    });
+      it(`${type} (Motorcycle) has 2 wheels`, () => {
+        expect(motorcycle.wheels).toEqual(2);
+      });
 
-    it('cannot do a wheelie', () => {
-      expect(motorcycle.wheelie()).toBeTruthy();
+      it(`${type} (Motorcycle) can drive`, () => {
+        expect(motorcycle.drive()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) can stop`, () => {
+        expect(motorcycle.stop()).toBeTruthy();
+      });
+
+      it(`${type} (Motorcycle) cannot do a wheelie`, () => {
+        expect(motorcycle.wheelie()).toBeTruthy();
+      });
+      
     });
 
   });
